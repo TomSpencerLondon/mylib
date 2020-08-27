4199c8bc48fe54d09de8795bfdab710db08442dc
Tom <thomas.spencer@codurance.com>
2020-08-27 09:23:15 +0100

    Add betterReverse

--- src/Reverse.hs
+++ src/Reverse.hs
@@ -1,9 +1,18 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Reverse
-  ( myReverse
+  ( myReverse,
+  betterReverse
   ) where
 import RIO
 
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
+
+
+betterReverse :: [a] -> [a]
+betterReverse =
+    loop []
+  where
+    loop res [] = res
+    loop res (x:xs) = loop (x:res) xs
--- test/ReverseSpec.hs
+++ test/ReverseSpec.hs
@@ -9,3 +9,7 @@ spec = do
   describe "myReverse" $ do
     it "handles empty lists" $ myReverse [] `shouldBe` ([] :: [Int])
     it "reverses hello" $ myReverse "hello" `shouldBe` "olleh"
+
+  describe "betterReverse" $ do
+    prop "behaves the same as myReverse" $ \list ->
+      betterReverse list `shouldBe` myReverse (list :: [Int])
